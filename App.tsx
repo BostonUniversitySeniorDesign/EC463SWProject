eb4c4840b6dd309ec74ae8268df1b029f918db7f
scuterez00 <55964933+scuterez00@users.noreply.github.com>
2021-09-07 18:16:16 -0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
+
 const Separator = () => (
   <View style={styles.separator} />
 );
