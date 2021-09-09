f2a578a9078bd207840999eba8a187c97e2c3091
jcw0525 <jcw0525@bu.edu>
2021-09-09 15:31:39 -0400

    added functionality to query the fda database; todo: parse through .json - also! make sure to run npm install axios

--- FDAAPI.tsx
+++ FDAAPI.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import axios from 'axios';
 
 class FDAAPI{}
 
 FDAAPI.getCalories = function(FDC_ID)
 {
-	return 250;
+	axios.post
+	(
+		'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=cut3y5UH1MWJYdvRtN3gz74DT1YOxRvprPLWCdA1',
+		{query: FDC_ID, dataType: ["Branded"], sortBy: "fdcId", sortOrder: "desc"}
+	)
+	.then(function(response)
+	{
+		console.log(response.data);
+		return 'Calories';
+	})
+	.catch(function(error)
+	{
+		alert(error.message);
+	});
+	
+	
+	return 'Error';
 }
 
 module.exports = {
