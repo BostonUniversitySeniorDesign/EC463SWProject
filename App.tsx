3d481f4e1872241290af1b4891ca7bd268e76193
Stephanie Cuterez <scuterez@bu.edu>
2021-09-14 01:54:31 -0400

    First commit

--- App.tsx
+++ App.tsx
@@ -1,23 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Alert} from 'react-native'
+import {Camera} from 'expo-camera'
+let camera: Camera
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  
+  const [startCamera,setStartCamera] = React.useState(false);
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+  const __startCamera = async () => {
+    const {status} = await Camera.requestPermissionsAsync()
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
   }
+
+  return (
+    <View style={styles.container}>
+    {startCamera ? (
+        <Camera
+          style={{flex: 1,width:"100%"}}
+          ref={(r) => {
+            camera = r
+          }}
+        ></Camera>
+      ) : (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+          onPress={__startCamera}
+          style={{
+            width: 200,
+            borderRadius: 1,
+            backgroundColor: '#000',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 50
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.space} />
+
+        <TouchableOpacity
+          onPress={() => Alert.alert('Going to recipes')}
+          style={{
+            width: 200,
+            borderRadius: 1,
+            backgroundColor: '#000',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 50
+
+
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Recipes
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )}
+
+      <StatusBar style="auto" />
+    </View>
+  )
 }
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  space: {
+    width: 20, // or whatever size you need
+    height: 20,
+  },
+})
