b88538f2571bb558baa77aebad521dc9fccdd20f
jcw0525 <jcw0525@bu.edu>
2021-09-15 13:18:37 -0400

    added camera

--- App.tsx
+++ App.tsx
@@ -1,56 +1,142 @@
-import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Alert, Button} from 'react-native'
+import {Camera} from 'expo-camera'
+import { BarCodeScanner } from 'expo-barcode-scanner';
+let camera: Camera
 
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      
-      <Button
-        title="Camera"
-        onPress={() => Alert.alert('Going to camera')}
-      />
+export default function App() {
+  
+  const [startCamera,setStartCamera] = React.useState(false);
+  const [scanned, setScanned] = React.useState(false);
+  const [text, setText] = React.useState('Not yet scanned')
+
+  const __startCamera = async () => {
+    const {status} = await BarCodeScanner.requestPermissionsAsync()
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  }
+
+  return (
+    <View style={styles.container}>
+    {startCamera ? (
+      <View style={styles.container}>
+        <View 
+          style={styles.barcodebox}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={{ height: 400, width: 400 }} />
+        </View>
+        <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button 
+        title='Scan again?' 
+        onPress={() => setScanned(false)} color='#dc143c' />}
     </View>
-    <Separator />
-    <View>
-      <Button
-        title="Recipes"
-        
-        onPress={() => Alert.alert('Going to recipes')}
-      />
+      ) : (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+          onPress={__startCamera}
+          style={{
+            width: 200,
+            borderRadius: 1,
+            backgroundColor: '#000',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 50
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+          Scan Barcode 
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.space} />
+
+        <TouchableOpacity
+          onPress={() => Alert.alert('Going to recipes')}
+          style={{
+            width: 200,
+            borderRadius: 1,
+            backgroundColor: '#000',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 50
+
+
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Recipes
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )}
+
+      <StatusBar style="auto" />
     </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-      
-      </Text>
-    </View>
-  </SafeAreaView>
-);
+  )
+}
+
+
+
+
 
 const styles = StyleSheet.create({
+  maintext: {
+    fontSize: 30,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 400,
+    width: 350,
+    overflow: 'hidden',
+    borderRadius: 2,
+    backgroundColor: '#000'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
-
-export default App;
+  space: {
+    width: 20, 
+    height: 20,
+  }
+})
