19a1f82f125f4f514d35571a731ffb34dd7a3a52
jcw0525 <jcw0525@bu.edu>
2021-09-15 12:06:52 -0400

    added two functions: one to parse the barcode; another to return the calorie data

--- FDAAPI.tsx
+++ FDAAPI.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 import axios from 'axios';
 
-class FDAAPI{}
-
-FDAAPI.getCalories = function(FDC_ID)
+class FDAAPI
 {
+	static calories = "No Barcode";
+	static data = "No Data";
+}
+
+FDAAPI.parseBarcode = function(FDC_ID)
+{
+	this.calories = "Error: Still Fetching Data";
 	axios.post
 	(
 		'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=cut3y5UH1MWJYdvRtN3gz74DT1YOxRvprPLWCdA1',
@@ -13,16 +18,27 @@ FDAAPI.getCalories = function(FDC_ID)
 	)
 	.then(function(response)
 	{
-		console.log(response.data);
-		return 'Calories';
+		FDAAPI.parseJson(response.data);
 	})
 	.catch(function(error)
 	{
 		alert(error.message);
 	});
-	
-	
-	return 'Error';
+}
+
+FDAAPI.parseJson = function(data)
+{
+	console.log(data);
+	this.data = "data";
+	this.calories = "kCal";
+}
+
+FDAAPI.getCalories = function()
+{
+	const cal = this.calories;
+	this.calories = "No Barcode";
+	this.data = "No Data";
+	return cal;
 }
 
 module.exports = {
